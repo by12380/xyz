d89314586284d711a475382998158f71f36f5e53
Brian Young <by12380@gmail.com>
2018-11-19 14:27:05 -0800

    Update configs

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || 'react-universal.auth0.com';
-export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '3821inroTsXub5iKOew7JHTT-OaJl3jp';
-export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE || 'https://api.react-universal.com';
+export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '3S7QQM5n558htomG2YGlcibAq33790qi';
+export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE || 'https://api.xyz.com';
 export const AUTH0_LOGIN_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGIN_REDIRECT_URL || 'http://localhost:3000/callback';
 export const AUTH0_LOGOUT_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGOUT_REDIRECT_URL || 'http://localhost:3000/';
 
